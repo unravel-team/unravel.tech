615c641ab007ff5b7a4a086de3637dec3ea08904
Kapil Reddy <reddy.kapil@gmail.com>
2025-11-30 17:43:23 +0530

    Fix local dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import icon from 'astro-icon';
-
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), icon()],
   output: 'server',
-  vite: {
-    server: {
-      allowedHosts: ['.ngrok-free.app', '.ngrok.io', '.ngrok.app'],
-    },
-    ssr: {
-      noExternal: ['react', 'react-dom'],
-    },
-  },
-
   adapter: cloudflare({
     platformProxy: { enabled: true },
   }),
